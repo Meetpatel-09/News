f1ab0895c770feda4f60ead5455f6b0208a866ff
Meet Patel <pmeet007m@gmail.com>
2021-03-28 00:00:22 +0530

    Day 1.1

--- App_Code/Class1.cs
+++ App_Code/Class1.cs
@@ -2,16 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
 public class Class1
 {
-	public Class1()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	}
+    public static string sc = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString.ToString();
+
+    public static int exQuery(string sqlquery)
+    {
+        SqlConnection con = new SqlConnection(sc);
+        con.Open();
+        SqlCommand cmd = new SqlCommand(sqlquery, con);
+        int i;
+        i = cmd.ExecuteNonQuery();
+        return i;
+        con.Close();
+    }
+
+    public static DataSet select(string sqlQuery)
+    {
+        SqlConnection con = new SqlConnection(sc);
+        con.Open();
+        SqlCommand cmd = new SqlCommand(sqlQuery, con);
+        DataSet ds = new DataSet();
+        SqlDataAdapter adp = new SqlDataAdapter(cmd);
+        adp.Fill(ds);
+        return ds;
+        con.Close();
+    }
 }
